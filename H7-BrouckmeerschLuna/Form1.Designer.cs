6cda1bcae83ca1b52925e803099c0256dd0369d7
Luna Brouckmeersch <luna.brouckmeersch@student.ppsjo.be>
2023-04-19 11:48:25 +0200

    wijziging button

--- H7-BrouckmeerschLuna/Form1.Designer.cs
+++ H7-BrouckmeerschLuna/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(236, 113);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(310, 178);
